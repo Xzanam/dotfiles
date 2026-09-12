93c9a9668ec682dd052b407e752913645c663afe
Janam Shrestha <sznam77@gmail.com>
2026-09-13 01:59:46 +0545

    autostart changes

--- .config/hypr/conf/autostart.lua
+++ .config/hypr/conf/autostart.lua
@@ -15,20 +15,20 @@ hl.permission("/usr/(bin|local/bin)/hyprpm", "plugin", "allow")
 -- Autostart
 -- See https://wiki.hypr.land/Configuring/Basics/Autostart/
 hl.on("hyprland.start", function()
-    hl.exec_cmd("hyprpm reload -n")
+	hl.exec_cmd("hyprpm reload -n")
 
-    -- xdg-desktop-portal-hyprland for screensharing
-    hl.exec_cmd("~/.config/hypr/xdg-portal-hyprland")
-    hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
-    hl.exec_cmd("systectl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP QT_QPA_PLATFORMTHEME")
-    -- hl.exec_cmd("waybar")
-    hl.exec_cmd("qs -c noctalia-shell")
-    -- hl.exec_cmd("swaync")
-    hl.exec_cmd("discord")
-    -- hl.exec_cmd("hyprpaper")
-    -- hl.exec_cmd("swaybg -m fill -i ~/Pictures/Wallpapers/pkr.jpg")
+	-- xdg-desktop-portal-hyprland for screensharing
+	hl.exec_cmd("~/.config/hypr/xdg-portal-hyprland")
+	hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+	hl.exec_cmd("systectl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP QT_QPA_PLATFORMTHEME")
+	-- hl.exec_cmd("waybar")
+	hl.exec_cmd("noctalia")
+	-- hl.exec_cmd("swaync")
+	hl.exec_cmd("discord")
+	-- hl.exec_cmd("hyprpaper")
+	-- hl.exec_cmd("swaybg -m fill -i ~/Pictures/Wallpapers/pkr.jpg")
 
-    -- Clipboard manager
-    hl.exec_cmd("wl-paste --type text --watch cliphist store")
-    hl.exec_cmd("wl-paste --type image --watch cliphist store")
+	-- Clipboard manager
+	hl.exec_cmd("wl-paste --type text --watch cliphist store")
+	hl.exec_cmd("wl-paste --type image --watch cliphist store")
 end)
